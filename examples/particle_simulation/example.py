13c398e9689eaabaed5988b39de8c8e154aa679b
LandonHarter <landonharter@outlook.com>
2025-02-12 01:25:43 -0500

    more customization with particle sim

--- examples/particle_simulation/example.py
+++ examples/particle_simulation/example.py
@@ -5,7 +5,7 @@ from manim_kodisc import *
 
 class ParticleSimulationExample(Scene):
     def construct(self):
-        sim = ParticleSimulation(n_particles=100, container_dimensions=(6, 4))
+        sim = ParticleSimulation(n_particles=30, container_dimensions=(2, 2), particle_colors=[PURPLE, ORANGE, GREEN])
         sim.move_to(ORIGIN)
         self.add(sim)
-        self.play(UpdateFromFunc(sim, lambda x: x.update_particles()), run_time=10)
+        self.play(UpdateFromFunc(sim, lambda x: x.update_particles(1 / config.frame_rate)), run_time=10)
