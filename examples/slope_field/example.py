4f7e6e159aab6b6389e5ca57c61cf47fd12f4160
LandonHarter <landonharter@outlook.com>
2025-05-17 11:56:14 -0400

    fix slope fields

--- examples/slope_field/example.py
+++ examples/slope_field/example.py
@@ -6,7 +6,7 @@ from manim_kodisc import *
 class SlopeFieldExample(Scene):
     def construct(self):
         slope_field = SlopeField(
-            lambda x: np.sin(x),
+            lambda x: np.cos(x),
             x_range=(-5, 5),
             y_range=(-5, 5),
             step_size=0.5,
@@ -14,6 +14,6 @@ class SlopeFieldExample(Scene):
         self.play(Write(slope_field))
 
         self.wait(0.5)
-        self.play(Write(slope_field.axes.plot(slope_field.func, color=BLUE)))
+        self.play(Write(slope_field.axes.plot(lambda x: np.sin(x), color=BLUE)))
 
         self.wait(5)
